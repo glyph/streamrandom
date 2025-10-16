75758794e14f2c1b3f498b8c77a9ece4b10f1310
Glyph <code@glyph.im>
2025-10-15 18:58:47 -0700

    tox.ini, tests
    This commit was sponsored by Blaise Pabon, Jacob Kaplan-Moss, Carlton
    Gibson, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- test_streamrandom.py
+++ test_streamrandom.py
@@ -2,6 +2,7 @@ from streamrandom import stream_from_seed
 
 from unittest import TestCase
 
+
 class Tests(TestCase):
     """
     Some tests.
@@ -10,10 +11,9 @@ class Tests(TestCase):
     def test_read_vector1(self):
         stream = stream_from_seed("this is a test")
         stream.read(100)
-        self.assertEqual(stream.read(10), b'\xd8-/w\xd3\xdaY5\xd3\x85')
-
+        self.assertEqual(stream.read(10), b"\xd8-/w\xd3\xdaY5\xd3\x85")
 
     def test_seek_vector1(self):
         stream = stream_from_seed("this is a test")
         stream.seek(100)
-        self.assertEqual(stream.read(10), b'\xd8-/w\xd3\xdaY5\xd3\x85')
+        self.assertEqual(stream.read(10), b"\xd8-/w\xd3\xdaY5\xd3\x85")
